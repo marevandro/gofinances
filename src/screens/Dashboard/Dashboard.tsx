65e1841cdcf413aac1aef2396f852ae64ca440bc
marevandro <marevandro95@gmail.com>
2024-10-28 12:16:01 -0300

    initial commit

---
+++ src/screens/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+export default function Dashboard (){
+  return(
+    <Container>
+      <Title>
+        Meua APP React Native com Expo
+      </Title>
+    </Container>
+  )
+}
+
