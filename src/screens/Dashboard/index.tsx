c174208a91958d690c2f6add5b9dbc1158390fd6
marevandro <marevandro95@gmail.com>
2024-10-31 21:32:52 -0300

    feat: add list card

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -9,11 +9,45 @@ import {
   UserGreeting,
   UserName,
   Icon,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList
 } from './styles';
 import { HighligthCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
 
 export default function Dashboard() {
+  const data = [
+    {
+      title: "Desenvolvimento de Site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: 'Vedas',
+        icon: 'dollar-sign'
+      },
+      date: '14/10/2024'
+    },
+    {
+      title: "Desenvolvimento de Site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: 'Vedas',
+        icon: 'dollar-sign'
+      },
+      date: '14/10/2024'
+    },
+    {
+      title: "Desenvolvimento de Site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: 'Vedas',
+        icon: 'dollar-sign'
+      },
+      date: '14/10/2024'
+    }
+  ];
+
   return (
     <Container>
       <Header>
@@ -29,26 +63,35 @@ export default function Dashboard() {
         </UserWrapper>
       </Header>
 
-
       <HighlightCards>
-        <HighligthCard 
-          title='Entradas' 
-          amount='R$ 17.400,00' 
+        <HighligthCard
+          title='Entradas'
+          amount='R$ 17.400,00'
           lasTransaction='Última entrada dia 30 de Outubro'
-          type="up"/>
+          type="up" />
 
-        <HighligthCard 
-        title='Saídas' 
-        amount='R$ 1.259,00' 
-        lasTransaction='Última saída dia 21 de Outubro'
-        type="down"/>
-        
-        <HighligthCard 
-        title='Total' 
-        amount='R$ 16.141,00' 
-        lasTransaction='01 á 31 de Outubro'
-        type='total'/>
+        <HighligthCard
+          title='Saídas'
+          amount='R$ 1.259,00'
+          lasTransaction='Última saída dia 21 de Outubro'
+          type="down" />
+
+        <HighligthCard
+          title='Total'
+          amount='R$ 16.141,00'
+          lasTransaction='01 á 31 de Outubro'
+          type='total' />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionList
+          data={data}
+          renderItem={({ item }) => <TransactionCard data={item} />} 
+          showsVerticalScrollIndicator={false}/>
+
+      </Transactions>
     </Container>
   )
 }
